9163f586c1e17db10a720c27a02b9a47e81d12ca
toniher <toniher@cau.cat>
2015-08-05 17:15:49 +0200

    adding some stuff

---
+++ SemanticFormsSelect.api.php
@@ -0,0 +1,41 @@
+<?php
+class ApiSemanticFormsSelect extends ApiBase {
+
+	public function execute() {
+
+		$params = $this->extractRequestParams();
+
+		return true;
+
+	}
+
+	public function getAllowedParams() {
+		return array(
+			'approach' => array(
+				ApiBase::PARAM_TYPE => 'string',
+				ApiBase::PARAM_REQUIRED => true
+			),
+			'query' => array(
+				ApiBase::PARAM_TYPE => 'string',
+				ApiBase::PARAM_REQUIRED => true
+			)
+		);
+	}
+
+	public function getDescription() {
+		return array(
+			'API for providing SemanticFormsSelect values'
+		);
+	}
+	public function getParamDescription() {
+		return array(
+			'approach' => 'The actual approach: function or smw',
+			'query' => 'The query of the former'
+		);
+	}
+
+	public function getVersion() {
+		return __CLASS__ . ': 1.1';
+	}
+
+}
