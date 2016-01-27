f1a4b2335b6883f46290eb318b31900da49ce29e
paladox <paladox@users.noreply.github.com>
2016-01-27 09:08:17 +0000

    Update SemanticFormsSelect.php

--- SemanticFormsSelect.php
+++ SemanticFormsSelect.php
@@ -23,8 +23,10 @@ if ( isset( $wgWikimediaTravisCI ) && $wgWikimediaTravisCI == true ) {
 }
 
 $GLOBALS['wgExtensionFunctions'][] = function() {
-	if ( !defined( 'SF_VERSION' ) || version_compare( 'SF_VERSION', '2.8', 'lt' ) ) {
-	   die( '<b>Error:</b> This version of <a href="https://github.com/SemanticMediaWiki/SemanticFormsSelect/">SemanticFormsSelect</a> is only compatible with Semantic Forms 2.8 or above. You need to upgrade <a href="https://www.mediawiki.org/wiki/Extension:Semantic_Forms">Semantic Forms</a> first.' );
+	if ( version_compare( $GLOBALS['wgVersion'], '1.25c', '<' ) ) {
+		if ( !defined( 'SF_VERSION' ) || version_compare( 'SF_VERSION', '2.8', '<' ) ) {
+		   die( '<b>Error:</b> This version of <a href="https://github.com/SemanticMediaWiki/SemanticFormsSelect/">SemanticFormsSelect</a> is only compatible with Semantic Forms 2.8 or above. You need to upgrade <a href="https://www.mediawiki.org/wiki/Extension:Semantic_Forms">Semantic Forms</a> first.' );
+		}
 	}
 
 	if ( version_compare( $GLOBALS['wgVersion'], '1.26c', '>' ) ) {
