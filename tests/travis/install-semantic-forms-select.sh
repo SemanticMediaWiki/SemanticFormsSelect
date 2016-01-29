81f8b9eab9081605c5d99bad24af023c9b0360da
paladox <paladox@users.noreply.github.com>
2016-01-29 20:22:25 +0000

    Update install-semantic-forms-select.sh

--- tests/travis/install-semantic-forms-select.sh
+++ tests/travis/install-semantic-forms-select.sh
@@ -23,7 +23,7 @@ function installToMediaWikiRoot {
 	else
 composer update
 composer init --stability dev
-composer require "mediawiki/semantic-forms-select:dev-master" --prefer-source --dev
+composer require "mediawiki/semantic-forms-select:dev-master" --prefer-source --no-update --dev
 		cd extensions
 		cd SemanticFormsSelect
 composer update --dev
