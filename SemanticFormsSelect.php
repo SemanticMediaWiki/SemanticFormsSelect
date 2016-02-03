2e37ca63c10fc554e2bd440480e4454c15b37088
paladox <paladox@users.noreply.github.com>
2016-02-03 17:41:29 +0000

    Update SemanticFormsSelect.php

--- SemanticFormsSelect.php
+++ SemanticFormsSelect.php
@@ -18,6 +18,17 @@ if ( version_compare( $GLOBALS[ 'wgVersion' ], '1.23', 'lt' ) ) {
 	die( '<b>Error:</b> This version of <a href="https://github.com/SemanticMediaWiki/SemanticFormsSelect/">SemanticFormsSelect</a> is only compatible with MediaWiki 1.23 or above. You need to upgrade MediaWiki first.' );
 }
 
+// A workaround to stay compatible with the 3.4.x release.
+if ( defined( 'SMW_VERSION' ) ) {
+	$GLOBALS['wgExtensionFunctions'][] = function() {
+		// This global variable is needed so that other extensions can
+		// hook into it to add their own input types.
+		$GLOBALS['sfgFormPrinter'] = new StubObject( 'sfgFormPrinter', 'SFFormPrinter' );
+	};
+} else {
+	$GLOBALS['sfgFormPrinter'] = new StubObject( 'sfgFormPrinter', 'SFFormPrinter' );
+}
+
 $GLOBALS['wgExtensionFunctions'][] = function() {
 	if ( version_compare( SF_VERSION, '2.8', '<' ) ) {
 		die( '<b>Error:</b> This version of <a href="https://github.com/SemanticMediaWiki/SemanticFormsSelect/">SemanticFormsSelect</a> is only compatible with Semantic Forms 2.8 or above. You need to upgrade <a href="https://www.mediawiki.org/wiki/Extension:Semantic_Forms">Semantic Forms</a> first.' );
