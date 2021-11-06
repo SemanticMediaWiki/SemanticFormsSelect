d26ae413a3cf1e8d4247598d9bf161928ea408c4
Mark A. Hershberger <mah@nichework.com>
2021-11-05 20:16:44 -0400

    Remove commented out code in bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,20 +10,15 @@ ini_set( 'display_errors', 1 );
 
 global $IP;
 
-//if ( !is_readable( $autoloaderClassPath = __DIR__ . '/../../SemanticMediaWiki/tests/autoloader.php' ) ) {
-if ( !is_readable( $autoloaderClassPath = $IP . '/extensions/SemanticMediaWiki/tests/autoloader.php' ) ) {
+if ( !is_readable(
+	$autoloaderClassPath = $IP . '/extensions/SemanticMediaWiki/tests/autoloader.php'
+) ) {
 	die( "\nThe Semantic MediaWiki test autoloader is not available\n" );
 }
 
-// if ( !class_exists( 'SemanticFormsSelect' ) || ( $version = SemanticFormsSelect::getVersion() ) === null ) {
 if ( ExtensionRegistry::getInstance()->isLoaded( 'SemanticFormsSelect' ) ) {
 	die( "\nSemantic Forms Select is not available, please check your Composer or LocalSettings.\n" );
 }
 
-//print sprintf( "\n%-20s%s\n", "Semantic Forms Select: ", ExtensionRegistry::getInstance()->isLoaded( 'SemanticFormsSelect' ) );
-//print sprintf( "%-20s%s\n", "Page Forms: ", SemanticFormsSelect::getVersion( 'PageForms' ) );
-
 $autoloader = require $autoloaderClassPath;
-//$autoloader->addPsr4( 'SFS\\Tests\\', __DIR__ . '/phpunit/Unit' );
-//$autoloader->addPsr4( 'SFS\\Tests\\Integration\\', __DIR__ . '/phpunit/Integration' );
 unset( $autoloader );
