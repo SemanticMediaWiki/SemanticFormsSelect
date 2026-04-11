0e4e51637b9c5bd4f1f1359137dc77ac08d1bfff
paladox <paladox@users.noreply.github.com>
2026-04-12 00:02:34 +0100

    Update ApiSemanticFormsSelectTest.php

--- tests/phpunit/Unit/ApiSemanticFormsSelectTest.php
+++ tests/phpunit/Unit/ApiSemanticFormsSelectTest.php
@@ -2,11 +2,11 @@
 
 namespace SFS\Tests;
 
+use MediaWiki\Api\ApiMain;
+use MediaWiki\Context\RequestContext;
+use MediaWiki\Request\WebRequest;
+use MediaWiki\Request\FauxRequest;
 use SFS\ApiSemanticFormsSelect;
-use ApiMain;
-use RequestContext;
-use WebRequest;
-use FauxRequest;
 
 /**
  * @covers  \SFS\ApiSemanticFormsSelect
